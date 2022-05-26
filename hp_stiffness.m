a0ccb12384ea24d28e21c0ce269378baab37b5a5
truptimr <trupti.ranka@gmail.com>
2022-05-26 16:48:03 -0700

    Test simulation at orientation 0,0,0

--- hp_stiffness.m
+++ hp_stiffness.m
@@ -1,12 +1,12 @@
 function [Fh,Kh,Fhbk] = hp_stiffness(Xh,Xha)
-Fhbp = 1000; %M1B coodinate system limits. tension
+Fhbp = 1600; %M1B coodinate system limits. tension
 Fhbn = -1600;% compression
 K = 120e6;
 Kh = K*ones(6,1);
 Fhbk = zeros(6,1);
 Fh = -K*(Xh - Xha);
-indp = Fh> Fhbp;
-indn = Fh< Fhbn;
+indp = Fh>= Fhbp;
+indn = Fh<= Fhbn;
 Kh(indp) = 0;
 Kh(indn) = 0;
 Fhbk(indp) = Fhbp;
